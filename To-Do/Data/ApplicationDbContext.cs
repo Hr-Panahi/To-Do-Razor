a3a70b29d0ab6dcaa5fe303c622c36837798ee5d
Hamid Reza Panahi <110527736+Hr-Panahi@users.noreply.github.com>
2024-02-24 20:18:15 +0330

    Initial Configuration
    Configured database and created table for ToDoTask

---
+++ To-Do/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using To_Do.Model;
+namespace To_Do.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<ToDoTask> ToDoTask { get; set; }
+
+
+    }
+}
+ 
